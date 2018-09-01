8285d0b42d086c6ce783872a69746cfde81280f7
Daniel Jarai <jaraidaniel@gmail.com>
2018-09-02 04:04:00 +0530

    lettuce: Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81

--- device.mk
+++ device.mk
@@ -64,6 +64,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
