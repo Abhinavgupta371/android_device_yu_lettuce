bc673539a9d355127cf35958cd57fd423b6e7e4d
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-10-02 10:17:18 +0000

    lettuce: Set cryptfs_hw path
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,7 @@ USE_OPENGL_RENDERER := true
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_LEGACY_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/cryptfs_hw
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # Extended Filesystem Support
