f32068322bff10ac334c18218f49489f986557ce
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-10-13 09:21:56 +0530

    lettuce: Bring back sepolicy !

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,9 +197,8 @@ TARGET_DISABLE_OTA_ASSERT := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#include device/qcom/sepolicy/legacy-sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+include device/qcom/sepolicy-legacy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
