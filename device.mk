b2242c23a51c913b4d578ae55bcf29a40fe8096d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2018-11-01 15:27:55 +0530

    lettuce: Update FM radio for pie
    * Rename FM initialization prop for the new HAL
    * Build broadcastradio HIDL impl

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/Bluetooth_cal.acdb:system/etc/acdbdata/QRD/Bluetooth_cal.acdb \
