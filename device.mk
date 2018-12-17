8bbe83270846763904fa0f57af0c1823bab8cb3e
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    lettuce: Uprev audio HAL to v5
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- device.mk
+++ device.mk
@@ -29,9 +29,10 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/Bluetooth_cal.acdb:system/etc/acdbdata/QRD/Bluetooth_cal.acdb \
