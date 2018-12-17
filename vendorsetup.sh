d80461445897151988493ceb387e9c6a605c3a26
Adithya R <gh0strider.2k18.reborn@gmail.com>
2018-12-17 10:06:03 +0530

    lettuce: vendorsetup: Switch to GCC 8.x for compiling kernel
    * Supported RebornKernel versions: R5.5 and above

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,29 +6,30 @@ export SDCLANG=false
 #export SDCLANG_FLAGS_2="-O3 -fvectorize -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
 #export SDCLANG_COMMON_FLAGS="-O3 -fvectorize -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
 
-GCC_7_DIR=prebuilts/gcc/linux-x86/aarch64/aarch64-opt-linux-android
-GCC_7_LINK="https://github.com/krasCGQ/aarch64-linux-android/"
+GCC_DIR=prebuilts/gcc/linux-x86/aarch64/aarch64-opt-linux-android
+GCC_LINK="https://github.com/krasCGQ/aarch64-linux-android/"
 
-if [ ! -d $GCC_7_DIR ]; then
+if [ ! -d $GCC_DIR ]; then
 
   tput bold
   tput setaf 1
-  echo -e "\nWARNING: GCC 7.x / Linaro 7.x not present at $GCC_7_DIR"
+  echo -e "\nWARNING: GCC 8.x not present at $GCC_DIR"
 
-  wget -q --spider http://google.com
+  wget -q --spider https://github.com
 
   if [ $? -eq 0 ]; then
     tput setaf 4
     echo -e "\nInternet connection detected"
-    echo -e "\n=======================\nCloning Linaro 7.x ....\n=======================\n"
-    git clone -b opt-linaro-7.x $GCC_7_LINK $GCC_7_DIR
+    echo -e "\n=======================\nCloning GCC 8.x ....\n=======================\n"
+    git clone -b opt-gnu-8.x $GCC_LINK $GCC_DIR
   else
     tput setaf 1
-    echo -e "You are not connected to the internet! Cannot clone Linaro 7.x automatically\n"
-    echo -e "ERROR: Please clone Linaro 7.x from $GCC_7_LINK (opt-linaro-7.x branch) \nto $GCC_7_DIR, before continuing build !!\n"
-    echo -e "Or else if it is present elsewhere, update the dir in BoardConfig.mk."
+    echo -e "You are not connected to the internet! Cannot clone GCC 8.x toolchain automatically\n"
+    echo -e "ERROR: Please clone GCC 8.x from $GCC_LINK (opt-gnu-8.x branch) \nto $GCC_DIR, before continuing build !!\n"
+    tput sgr0
+    exit 1
   fi
-  
+
   tput sgr0
 
 fi
