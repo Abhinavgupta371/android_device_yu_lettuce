33698432f42736e889985f3c1ebd19b4f199b684
Roshan Pius <rpius@google.com>
2019-03-06 11:23:18 -0800

    lettuce: Turn off SoftAP MAC randomization
    Device does not seem to support MAC reconfiguration.
    
    Bug: 127559063
    Test: Manual verification of toggling softap on/off.
    Change-Id: I9ae25b4d14a0c207188fae2c52ac7791b2061a40

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -231,6 +231,7 @@ TARGET_USES_QCOM_WCNSS_QMI := true
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 
 # Inherit from proprietary files
 include vendor/yu/lettuce/BoardConfigVendor.mk
