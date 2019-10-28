449f96e54146d027bafc1d5f377b46fae40d89d5
abhinavgupta371 <abhinavgupta371@gmail.com>
2019-10-28 17:02:24 +0530

    lettuce: Aosip

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lettuce.mk
+    $(LOCAL_DIR)/aosip_lettuce.mk
--- aosip_lettuce.mk
+++ aosip_lettuce.mk
@@ -21,11 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/yu/lettuce/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lettuce
-PRODUCT_NAME := lineage_lettuce
+PRODUCT_NAME := aosip_lettuce
 PRODUCT_BRAND := YU
 PRODUCT_MODEL := YU5010
 PRODUCT_MANUFACTURER := YU
